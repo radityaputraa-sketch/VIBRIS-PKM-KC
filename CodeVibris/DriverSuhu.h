93f5d1f419e8796210f583698f88473dfdf7099b
Thufail Bima Putra Raditya <radityathufail@gmail.com>
2026-07-11 17:11:17 +0700

    Refactor DriverSuhu.h for include guards
    Replaced include guards with #pragma once and added SharedTypes.h.

--- CodeVibris/DriverSuhu.h
+++ CodeVibris/DriverSuhu.h
@@ -1,5 +1,5 @@
-#ifndef vibris_suhu
-#define vibris_suhu
+// DriverSuhu.h
+#pragma once
 #include <Arduino.h>
+#include "SharedTypes.h" // Menyediakan akses ke struct SensorFeatures lintas file
 void TaskDriverSuhu(void *pvParameters);
-#endif 
