9aca50349075b03c85f30a18ac1964fa87935253
Thufail Bima Putra Raditya <radityathufail@gmail.com>
2026-07-11 21:39:12 +0700

    Update FFTProcessor.h

--- CodeVibris/FFTProcessor.h
+++ CodeVibris/FFTProcessor.h
@@ -1,1 +1,5 @@
+#pragma once
+#include "SharedTypes.h"
 
+void FFTProcessor_Init();
+void FFTProcessor_Process(VibrationBuffer *input, SensorFeatures *features, float *rpm_out, float *bandEnergies_out);
