a9b57a12202cb9d481de88bab5f12a577b83d22b
Thufail Bima Putra Raditya <radityathufail@gmail.com>
2026-07-11 17:08:32 +0700

    Update DriverGetaran.h with pragma once and includes
    Refactor header file to use #pragma once and include SharedTypes.h.

--- CodeVibris/DriverGetaran.h
+++ CodeVibris/DriverGetaran.h
@@ -1,5 +1,6 @@
-#ifndef DRIVER_GETARAN_H
-#define DRIVER_GETARAN_H
+// DriverGetaran.h
+#pragma once
+//Gunakan LIS3DH
 #include <Arduino.h>
+#include "SharedTypes.h" // Menyediakan akses ke VibrationBuffer dan SensorFeatures
 void TaskDriverGetaran(void *pvParameters);
-#endif
