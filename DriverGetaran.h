f4e92b84d45bd256d603cffcdb19aacaaf64e12e
Thufail Bima Putra Raditya <radityathufail@gmail.com>
2026-07-15 16:55:27 +0700

    Delete DriverGetaran.h

--- DriverGetaran.h
+++
@@ -1,6 +0,0 @@
-// DriverGetaran.h
-#pragma once
-//Gunakan LIS3DH
-#include <Arduino.h>
-#include "SharedTypes.h" // Menyediakan akses ke VibrationBuffer dan SensorFeatures
-void TaskDriverGetaran(void *pvParameters);
