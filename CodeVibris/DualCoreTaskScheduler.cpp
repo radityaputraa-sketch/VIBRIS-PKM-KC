296df0a3424cde0796f783c1c26e26dae4bf6cfd
Thufail Bima Putra Raditya <radityathufail@gmail.com>
2026-07-19 15:16:50 +0700

    Update DualCoreTaskScheduler.cpp

--- CodeVibris/DualCoreTaskScheduler.cpp
+++ CodeVibris/DualCoreTaskScheduler.cpp
@@ -13,6 +13,9 @@ static float latestBandEnergies[4] = {0.0f, 0.0f, 0.0f, 0.0f};
 QueueHandle_t Scheduler_GetVibrationQueue() {
     return vibrationQueue;
 }
+static float latestRmsX = 0.0f;
+static float latestRmsZ = 0.0f;
+static float latestRmsY = 0.0f;
 
 static void TaskFFTProcessor(void *pvParameters) {
     static VibrationBuffer incomingBuffer;
@@ -26,18 +29,25 @@ static void TaskFFTProcessor(void *pvParameters) {
         if (xQueueReceive(vibrationQueue, &incomingBuffer, portMAX_DELAY) == pdTRUE) {
             FFTProcessor_Process(&incomingBuffer, &fftLocalFeatures, &rpmResult, bandEnergies);
             latestRPM = rpmResult;
+            latestRmsX = incomingBuffer.rms_x_raw;   // 
+            latestRmsZ = incomingBuffer.rms_z_raw;
+            latestRmsY = incomingBuffer.rms_y_raw;   //
             for (int i = 0; i < 4; i++) latestBandEnergies[i] = bandEnergies[i];
 
             updateVibrationFeature(fftLocalFeatures.rms_getaran);
 
-            Serial.printf("[FFT] RPM=%.1f | Unbalance=%.2f | Misalign=%.2f\n",
-                          rpmResult, bandEnergies[0], bandEnergies[1]);
+            Serial.printf("[FFT] RPM=%.1f |  Y_RMS=%.4f | X_RMS=%.4f | Z_RMS=%.4f | Unbalance=%.2f | Misalign=%.2f\n",
+                          rpmResult, fftLocalFeatures.rms_getaran, latestRmsX, latestRmsZ, bandEnergies[0], bandEnergies[1]);
         }
     }
 }
-
+void Scheduler_GetLatestAxisRMS(float *xOut, float *yOut, float *zOut) {
+    *xOut = latestRmsX;
+    *yOut = latestRmsY;
+    *zOut = latestRmsZ;
+}
 void Scheduler_InitTasks() {
-    vibrationQueue = xQueueCreate(2, sizeof(VibrationBuffer));
+    vibrationQueue = xQueueCreate(1, sizeof(VibrationBuffer));
 
     xTaskCreatePinnedToCore(
         TaskFFTProcessor, "Task_FFT", STACK_TASK_FFT, NULL,
